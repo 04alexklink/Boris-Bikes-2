b4d188b56a1976c8927e36c2b8bea0b1706f1e4f
EC2 Default User <ec2-user@ip-172-31-19-83.eu-west-2.compute.internal>
2020-06-09 17:23:31 +0000

    one-liner rspec w release_bike method created

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,7 +1,6 @@
 require 'docking_station'
 
-
 describe DockingStation do
-  it 'returns dockingstation' do
-  end
+  it {should respond_to(:release_bike)}
+   # expect(DockingStation).to respond_to? release_bike
 end
