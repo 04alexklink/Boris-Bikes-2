afcf1f6f5472715db6b8eea9b9e834c42536b5c4
Alex Klink <alex_klink123@hotmail.co.uk>
2020-06-10 14:44:01 +0000

    Set up test for release_bike to produce a bike, and made it pass

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,19 +2,24 @@ require 'docking_station'
 require 'bike'
 
 describe DockingStation do
+  let(:station) {DockingStation.new}
   it {should respond_to(:release_bike)}
+  it "creates new instance of bike Class" do
+    expect(station.release_bike).to be_a Bike
+  end
+end
 
-  it {expect((:release_bike:working?).to be_true}
+
 
   #expect DockingStation.release_bike to output Bike.new
-  #expect
-  #then b) expects the bike to be working?
-
+ 
+ #then b) expects the bike to be working?
+  #it {expect((:release_bike:working?).to be_true}
+ 
   #Your test should check that you can call working? on the result of DockingStation.release_bike,
   #and that the result of doing so is true.
 
 
-end
 #
 # describe DockingStation.release_bike do
 #   it {should respond_to(:working?)}
