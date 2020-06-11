b76d42520595e1b30b240590c8d8305691075a7b
India Rex <face940@gmail.com>
2020-06-11 16:33:39 +0100

    completes section 11

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,21 +1,13 @@
 
 class DockingStation
   attr_reader :bike
+
   def release_bike
     Bike.new
   end
 
   def dock(bike)
-    #bike
-   # @bike = bike
-    #@array = [] 
-    #@array.push(@bike)
-    
-    #return "#{bike} docked"
+    @bike = bike
   end
-  
- # def bike_check
-    
-#  end
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,53 +2,32 @@ require 'docking_station'
 require 'bike'
 
 describe DockingStation do
-  let(:station) {DockingStation.new}
-  let(:bike) {Bike.new}
-  
-  it {should respond_to(:release_bike)}
-  
-  it "creates new instance of bike Class" do
-    expect(station.release_bike).to be_a Bike
-  end
    
-  it "checks instance of bike is working" do
-    expect((station.release_bike).working?).to be true
+  it {is_expected.to respond_to(:dock).with(1).argument }
+
+  it 'docks something' do
+    bike = Bike.new
+    expect(subject.dock(bike)).to eq bike
   end
 
-    it {should respond_to(:dock)}
+  # it { is_expected.to respond_to(:bike) } <-- old test for #bike
+  it 'returns docked bikes' do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to eq bike
+  end
+
+  # let(:station) {DockingStation.new}
+  # let(:bike) {Bike.new}
+  
+  # it {should respond_to(:release_bike)}
+  
+  # it "creates new instance of bike Class" do
+  #   expect(station.release_bike).to be_a Bike
+  # end
    
-    it "docks with a bike as an argument" do
-    expect(station).to respond_to(:dock).with(1).argument
-  end
-    
-  before do
-    station.dock(:bike)
-  end
-      it "should return the instance of bike" do
-    expect(station.dock(bike)).to be_an_instance_of Bike
-  end
+  # it "checks instance of bike is working" do
+  #   expect((station.release_bike).working?).to be true
+  # end
 
 end
-
-  #it "it does dock a bike" do
-  #  expect(station.dock(bike)).to eq("#{bike} docked")
-  #end
-
-
-
-
-
-  #expect DockingStation.release_bike to output Bike.new
- 
- #then b) expects the bike to be working?
-  #it {expect((:release_bike:working?).to be_true}
- 
-  #Your test should check that you can call working? on the result of DockingStation.release_bike,
-  #and that the result of doing so is true.
-
-
-#
-# describe DockingStation.release_bike do
-#   it {should respond_to(:working?)}
-#
-# end
